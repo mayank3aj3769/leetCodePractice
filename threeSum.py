704007aec14fcfba950a11d0aee4decc34692bc7
mayank3aj3769 <mayank.raj3769@gmail.com>
2022-11-09 01:15:54 -0500

    3 sum

--- threeSum.py
+++ threeSum.py
@@ -1,39 +1,70 @@
+'''
+Approach is :- for every i , find all pairs in remaining array whose sum of elements is -i .. Do this for entire list .
+Result would be a 2D list [[i,x,y],[i,x1,y1],....[i,xn,yn]...]
+
+'''
+
+
+'''
+Approach is :- for every i , find all pairs in remaining array whose sum of elements is -i .. Do this for entire list .
+Result would be a 2D list [[i,x,y],[i,x1,y1],....[i,xn,yn]...]
+
+'''
+
+
 class Solution:
-    def threeSum(self, nums):
+    def threeSum(self, nums): 
         res=[]
         for i in range(0,len(nums)):
-            if i!=len(nums)-1:
+            if i+1 < len(nums)-3:
                 target=0-nums[i]
+                if target==0:
+                    continue
                 temp=self.twoSum(nums[i+1:],target)
-                if temp !=None :
-                    temp.append(nums[i])
-                else:
-                    temp=[]
-                temp.sort()
-                res.append(temp)
+                if temp !=None  and temp!=[]:
                 
+                    for k in temp:
+                        k.append(nums[i])
+                        k[0]=nums[i+1+k[0]]
+                        k[1]=nums[i+1+k[1]]
+                        k.sort()   
+                    
+                    for x in temp:
+                        if x not in res:
+                            res.append(x)
+                else:
+                    continue
+            else:
+                if nums[-3]+nums[-2]+nums[-1]== 0:
+                    t=[nums[-3],nums[-2],nums[-1]]
+                    if t not in res:
+                        res.append(t)
+        
         return res
     
-    def twoSum(self,num,target):
-        for i in range(0,len(num)):
-            if i < len(num)-i:
-                t=target-num[i]
-                if t not in num[i+1:]:
+    def twoSum(self, nums,target):
+        ans=[]
+        for i in range(0,len(nums)):
+            res=[]
+            if i+1  < len(nums)-1:
+                temp=nums[i+1:]
+                if target-nums[i] not in temp:
                     continue
                 else:
-                    res=[]
-                    ind=num[i+1:].index(t)
+                    ind=nums[i+1:].index(target-nums[i])
                     res.append(i)
                     res.append(ind+i+1)
-                    return res
+                   
             else:
-                res=[]
-                if num[-1] == target-num[i]:
-                    res.append(i)
-                    res.append(len(num)-1)
-                    return res
+                temp=nums[-1] 
+                if temp == target-nums[i]:
+                    res.append(len(nums)-2)
+                    res.append(len(nums)-1)
                 
-                
+            if res not in ans and res!=[]:
+                ans.append(res)
+        return ans
 
+t=[3,0,-2,-1,1,2]
 sol=Solution()
-print(sol.threeSum([-1,0,1,2,-1,-4]))
+print(sol.threeSum(t))
